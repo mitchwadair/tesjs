62b3cc4129d9566edd7b246a338655c867b83d8c
Mitchell Adair <mitchwadair@gmail.com>
2020-11-19 16:39:24 -0500

    <events>: remove unnecessary code

--- src/events.js
+++ src/events.js
@@ -5,22 +5,11 @@
 
 class EventManager {
     constructor() {
-        // EventManager singleton
-        if (EventManager._instance)
-            return EventManager._instance;
-
-        if (!(this instanceof EventManager))
-            return new EventManager(config);
-
         EventManager._instance = this;
-
         this._events = this._events || {};
     }
 
     fire(type, data) {
-        if (!this._events)
-            this._events = {};
-    
         const handler = this._events[type];
         if (!handler) {
             console.log(`Recieved event for unhandled type: ${type}`);
@@ -35,17 +24,11 @@ class EventManager {
         if (typeof handler !== 'function')
             throw TypeError('Event handler must be a function');
     
-        if (!this._events)
-            this._events = {};
-    
         this._events[type] = handler;
         return this;
     }
 
     removeListener(type) {
-        if (!this._events)
-            return this;
-    
         if (this._events[type])
             delete this._events[type];
     
@@ -53,9 +36,6 @@ class EventManager {
     }
 
     removeAllListeners() {
-        if (!this._events)
-            return this;
-    
         this._events = {};
         return this;
     }
