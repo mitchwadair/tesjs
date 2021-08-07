3d1f47f67dd6700e175d28658ba08b59afda95c7
Mitchell Adair <mitchwadair@gmail.com>
2021-08-07 02:00:49 -0400

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const ne = require("webpack-node-externals");
 
 module.exports = {
-    entry: "./main.js",
+    entry: "./lib/tes.js",
     mode: "production",
     output: {
         path: path.resolve(__dirname, "dist"),
@@ -13,7 +13,7 @@ module.exports = {
             // don't include whserver in the browser package
             {
                 test: /whserver\.js/,
-                use: path.resolve(__dirname, "webpack/ignore.js"),
+                use: path.resolve(__dirname, "webpack/exclude.js"),
             },
         ],
     },
