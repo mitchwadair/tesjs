69698721fc29b31a4b0bf68bd578bfcdf9f6457d
Mitchell Adair <mitchwadair@gmail.com>
2021-08-07 01:47:36 -0400

    #33: switch to node-fetch, update for diff tokens

--- test/utils.js
+++ test/utils.js
@@ -2,37 +2,6 @@ const utils = require('../lib/utils');
 const should = require('chai').should();
 
 describe('utils', _ => {
-    describe('request()', _ => {
-        it('POST should get an OK response', done => {
-            const data = {
-                name: 'foo',
-                job: 'bar'
-            }
-            const headers = {
-                'Content-type': 'application/json; charset=UTF-8'
-            }
-            utils.request('POST', 'https://reqres.in/api/users', headers, data).then(data => {
-                data.name.should.eq('foo');
-                done();
-            });
-        });
-
-        it('GET should get an OK response', done => {
-            utils.request('GET', 'https://reqres.in/api/users/2').then(res => {
-                res.data.id.should.eq(2);
-                done();
-            });
-        });
-
-        it('DELETE should get OK response', done => {
-            utils.request('DELETE', 'https://reqres.in/api/users/2').then(data => {
-                console.log(data);
-                data.should.be.a('string').that.is.empty;
-                done();
-            });
-        });
-    });
-
     describe('objectShallowEquals()', _ => {
         it('should return true when two objects match', done => {
             const obj1 = {
