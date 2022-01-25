659287eeb093e114caa780debd8ba9e8e0c10af8
Mitchell Adair <mitchwadair@gmail.com>
2022-01-24 19:54:22 -0500

    prettier on lib

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mitchell Adair
-// 
+//
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-const tes = require('./lib/tes');
-module.exports = tes
+const tes = require("./lib/tes");
+module.exports = tes;
