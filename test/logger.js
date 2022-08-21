fee6a4b5771e2e1b6ea38df9fe4a99c9d2a09c6d
Mitchell Adair <mitchwadair@gmail.com>
2022-08-21 15:30:03 -0400

    #36: update log tests, prettier

--- test/logger.js
+++ test/logger.js
@@ -1,66 +1,59 @@
-const logger = require('../lib/logger');
-const sinon = require('sinon');
+const logger = require("../lib/logger");
+const sinon = require("sinon");
 
-describe('logger', _ => {
-    let spy;
+describe("logger", () => {
+    let stub;
 
-    beforeEach(done => {
-        spy = sinon.spy(console, 'log');
-        done();
+    beforeEach(async () => {
+        stub = sinon.stub(console, "log");
     });
-    afterEach(done => {
-        console.log.restore();
-        logger.setLevel('none');
-        done();
+    afterEach(async () => {
+        stub.restore();
+        logger.setLevel("none");
     });
 
-    it('should not log anything at none level', done => {
-        logger.setLevel('none');
-        logger.log('this is an info log');
-        logger.warn('this is a warn log');
-        logger.error('this is an error log');
-        logger.debug('this is a debug log');
-        sinon.assert.notCalled(spy);
-        done();
+    it("should not log anything at none level", async () => {
+        logger.setLevel("none");
+        logger.log("this is an info log");
+        logger.warn("this is a warn log");
+        logger.error("this is an error log");
+        logger.debug("this is a debug log");
+        sinon.assert.notCalled(stub);
     });
 
-    it('should only log info at info level', done => {
-        logger.setLevel('info');
-        logger.log('this is an info log');
-        logger.warn('this is a warn log');
-        logger.error('this is an error log');
-        logger.debug('this is a debug log');
-        sinon.assert.calledOnce(spy);
-        done();
+    it("should only log info at info level", async () => {
+        logger.setLevel("info");
+        logger.log("this is an info log");
+        logger.warn("this is a warn log");
+        logger.error("this is an error log");
+        logger.debug("this is a debug log");
+        sinon.assert.calledOnce(stub);
     });
 
-    it('should only log info and warn at warn level', done => {
-        logger.setLevel('warn');
-        logger.log('this is an info log');
-        logger.warn('this is a warn log');
-        logger.error('this is an error log');
-        logger.debug('this is a debug log');
-        sinon.assert.calledTwice(spy);
-        done();
+    it("should only log info and warn at warn level", async () => {
+        logger.setLevel("warn");
+        logger.log("this is an info log");
+        logger.warn("this is a warn log");
+        logger.error("this is an error log");
+        logger.debug("this is a debug log");
+        sinon.assert.calledTwice(stub);
     });
 
-    it('should only log info, warn and error at error level', done => {
-        logger.setLevel('error');
-        logger.log('this is an info log');
-        logger.warn('this is a warn log');
-        logger.error('this is an error log');
-        logger.debug('this is a debug log');
-        sinon.assert.calledThrice(spy);
-        done();
+    it("should only log info, warn and error at error level", async () => {
+        logger.setLevel("error");
+        logger.log("this is an info log");
+        logger.warn("this is a warn log");
+        logger.error("this is an error log");
+        logger.debug("this is a debug log");
+        sinon.assert.calledThrice(stub);
     });
 
-    it('should log all levels at debug level', done => {
-        logger.setLevel('debug');
-        logger.log('this is an info log');
-        logger.warn('this is a warn log');
-        logger.error('this is an error log');
-        logger.debug('this is a debug log');
-        sinon.assert.callCount(spy, 4);
-        done();
+    it("should log all levels at debug level", async () => {
+        logger.setLevel("debug");
+        logger.log("this is an info log");
+        logger.warn("this is a warn log");
+        logger.error("this is an error log");
+        logger.debug("this is a debug log");
+        sinon.assert.callCount(stub, 4);
     });
-})
+});
