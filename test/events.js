f03ad4b638825ad16a445ae66110b17f6ef36d36
Mitchell Adair <mitchwadair@gmail.com>
2022-08-21 15:36:20 -0400

    #36: update events tests, prettier

--- test/events.js
+++ test/events.js
@@ -1,76 +1,76 @@
-const EventManager = require('../lib/events');
-const should = require('chai').should();
+const { expect } = require("chai");
+const EventManager = require("../lib/events");
 
-beforeEach(done => {
+beforeEach(async () => {
     EventManager.removeAllListeners();
-    done();
 });
 
-describe('EventManager', _ => {
-    it('should not add a listener which is passed a non-function handler', done => {
-        should.not.exist(EventManager._events['test']);
-        EventManager.addListener.bind('test', 'not a function').should.throw(Error);
-        done();
+describe("EventManager", () => {
+    it("should not add a listener which is passed a non-function handler", async () => {
+        expect(EventManager._events["test"]).to.not.exist;
+        EventManager.addListener.bind("test", "not a function").should.throw(Error);
     });
 
-    it('adds new event listeners', done => {
-        should.not.exist(EventManager._events['test']);
-        EventManager.addListener('test', _ => {return});
-        EventManager._events['test'].should.be.a('function');
-        done();
-    });
-
-    it('removes individual listeners correctly', done => {
-        should.not.exist(EventManager._events['test']);
-        EventManager.addListener('test', _ => {return});
-        EventManager._events['test'].should.be.a('function');
-        EventManager.removeListener('test');
-        should.not.exist(EventManager._events['test']);
-        done();
-    });
-
-    it('does nothing when removing invalid listener', done => {
-        should.not.exist(EventManager._events['test']);
-        EventManager.removeListener('test');
-        should.not.exist(EventManager._events['test']);
-        done();
-    })
-
-    it('removes all listeners correctly', done => {
-        should.not.exist(EventManager._events['test']);
-        EventManager.addListener('test', _ => {return});
-        EventManager.addListener('test2', _ => {return});
-        EventManager._events['test'].should.be.a('function');
-        EventManager._events['test2'].should.be.a('function');
-        EventManager.removeAllListeners();
-        EventManager._events.should.be.an('object').that.is.empty;
-        done();
-    });
-
-    it('fires existing events correctly with correct arguments', done => {
-        should.not.exist(EventManager._events['test']);
-        const argData = {
-            arg1: 'test arg1',
-            arg2: 'test arg2'
-        }
-        let arg1Actual, arg2Actual;
-        EventManager.addListener('test', event => {
-            arg1Actual = event.arg1;
-            arg2Actual = event.arg2;
+    it("adds new event listeners", async () => {
+        expect(EventManager._events["test"]).to.not.exist;
+        EventManager.addListener("test", () => {
+            return;
         });
-        EventManager.fire({type: 'test'}, argData).should.eq(true);
-        arg1Actual.should.eq('test arg1');
-        arg2Actual.should.eq('test arg2');
-        done();
+        EventManager._events["test"].should.be.a("function");
     });
 
-    it('does not fire unknown events', done => {
-        should.not.exist(EventManager._events['test']);
-        const argData = {
-            arg1: 'test arg1',
-            arg2: 'test arg2'
-        }
-        EventManager.fire({type: 'test'}, argData).should.eq(false);
-        done();
+    it("removes individual listeners correctly", async () => {
+        expect(EventManager._events["test"]).to.not.exist;
+        EventManager.addListener("test", () => {
+            return;
+        });
+        EventManager._events["test"].should.be.a("function");
+        EventManager.removeListener("test");
+        expect(EventManager._events["test"]).to.not.exist;
     });
-})
+
+    it("does nothing when removing invalid listener", async () => {
+        expect(EventManager._events["test"]).to.not.exist;
+        EventManager.removeListener("test");
+        expect(EventManager._events["test"]).to.not.exist;
+    });
+
+    it("removes all listeners correctly", async () => {
+        expect(EventManager._events["test"]).to.not.exist;
+        EventManager.addListener("test", () => {
+            return;
+        });
+        EventManager.addListener("test2", () => {
+            return;
+        });
+        EventManager._events["test"].should.be.a("function");
+        EventManager._events["test2"].should.be.a("function");
+        EventManager.removeAllListeners();
+        EventManager._events.should.be.an("object").that.is.empty;
+    });
+
+    it("fires existing events correctly with correct arguments", async () => {
+        expect(EventManager._events["test"]).to.not.exist;
+        const argData = {
+            arg1: "test arg1",
+            arg2: "test arg2",
+        };
+        let arg1Actual, arg2Actual;
+        EventManager.addListener("test", ({ arg1, arg2 }) => {
+            arg1Actual = arg1;
+            arg2Actual = arg2;
+        });
+        EventManager.fire({ type: "test" }, argData).should.eq(true);
+        arg1Actual.should.eq("test arg1");
+        arg2Actual.should.eq("test arg2");
+    });
+
+    it("does not fire unknown events", async () => {
+        expect(EventManager._events["test"]).to.not.exist;
+        const argData = {
+            arg1: "test arg1",
+            arg2: "test arg2",
+        };
+        EventManager.fire({ type: "test" }, argData).should.eq(false);
+    });
+});
