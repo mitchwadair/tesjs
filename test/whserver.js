36c265583b3db395fa2e658e2b74f2062092b547
Mitchell Adair <mitchwadair@gmail.com>
2022-08-22 00:30:41 -0400

    #36: update whserver tests with twitch cli

--- test/whserver.js
+++ test/whserver.js
@@ -1,16 +1,22 @@
 const TES = require("../main");
 const request = require("supertest");
+const nock = require("nock");
+const sinon = require("sinon");
 const crypto = require("crypto");
+const { cmd } = require("./testUtil");
+const { expect } = require("chai");
 
 // example data taken from https://dev.twitch.tv/docs/eventsub examples
 
 const secret = "s3cRe7";
-const whSecret = "s3cRe7tW0";
+const whSecret = "s3cRe7tW0o";
 const timestamp = new Date().toISOString();
+
+const REDIRECT_URL = "http://localhost:8080/teswh/event";
 
 describe("whserver", () => {
     let tes, app;
-    before(() => {
+    before((done) => {
         TES._instance = null;
         tes = new TES({
             identity: {
@@ -21,123 +27,47 @@ describe("whserver", () => {
                 baseURL: "localhost",
                 secret: whSecret,
             },
+            options: { logging: false },
         });
         app = tes.whserver;
+        setTimeout(done, 500);
     });
 
     it("responds with 401 to request without twitch message signature", (done) => {
         request(app).post("/teswh/event").send({}).expect(401, done);
     });
 
-    it("responds with 403 to request with signature mismatch", (done) => {
-        request(app)
-            .post("/teswh/event")
-            .set({
-                "Twitch-Eventsub-Message-Id": "e76c6bd4-55c9-4987-8304-da1588d8988b",
-                "Twitch-Eventsub-Message-Retry": 0,
-                "Twitch-Eventsub-Message-Type": "webhook_callback_verification",
-                "Twitch-Eventsub-Message-Signature": "sha256=thisiswrong",
-                "Twitch-Eventsub-Message-Timestamp": timestamp,
-                "Twitch-Eventsub-Subscription-Type": "channel.follow",
-                "Twitch-Eventsub-Subscription-Version": 1,
-            })
-            .send({})
-            .expect(403, done);
+    it("responds with 403 to request with signature mismatch", async () => {
+        const out = await cmd(`twitch event trigger subscribe -F ${REDIRECT_URL} -s wrongsecret`);
+        expect(out).to.contain("Recieved Status Code: 403");
+        expect(out).to.contain("Request signature mismatch");
     });
 
     it("responds with challenge when challenged", (done) => {
-        const payload = {
-            challenge: "pogchamp-kappa-360noscope-vohiyo",
-            subscription: {
-                id: "f1c2a387-161a-49f9-a165-0f21d7a4e1c4",
-                status: "webhook_callback_verification_pending",
-                type: "channel.follow",
-                version: "1",
-                condition: {
-                    broadcaster_user_id: "12826",
-                },
-                transport: {
-                    method: "webhook",
-                    callback: "https://example.com/webhooks/callback",
-                },
-                created_at: "2019-11-16T10:11:12.123Z",
-            },
-        };
-        const signature = crypto
-            .createHmac("sha256", whSecret)
-            .update("e76c6bd4-55c9-4987-8304-da1588d8988b" + timestamp + Buffer.from(JSON.stringify(payload), "utf-8"))
-            .digest("hex");
-        request(app)
-            .post("/teswh/event")
-            .set({
-                "Twitch-Eventsub-Message-Id": "e76c6bd4-55c9-4987-8304-da1588d8988b",
-                "Twitch-Eventsub-Message-Retry": 0,
-                "Twitch-Eventsub-Message-Type": "webhook_callback_verification",
-                "Twitch-Eventsub-Message-Signature": `sha256=${signature}`,
-                "Twitch-Eventsub-Message-Timestamp": timestamp,
-                "Twitch-Eventsub-Subscription-Type": "channel.follow",
-                "Twitch-Eventsub-Subscription-Version": 1,
-            })
-            .send(payload)
-            .expect(200)
-            .end((err, res) => {
-                if (err) return done(err);
-                res.text.should.eq("pogchamp-kappa-360noscope-vohiyo");
-                done();
+        nock("https://api.twitch.tv")
+            .post("/helix/eventsub/subscriptions")
+            .reply(201, () => {
+                setTimeout(async () => {
+                    const out = await cmd(`twitch event verify channel.update -F ${REDIRECT_URL} -s ${whSecret}`);
+                    expect(out).to.contain("Valid response");
+                    done();
+                }, 100);
+                return {
+                    data: [{ id: 1 }],
+                };
             });
+
+        tes.subscribe("channel.update", { broadcaster_user_id: "1234" });
     });
 
-    it("responds with 200 OK when receiving a notification and the event should be fired", (done) => {
-        let notificationRecieved = false;
-        tes.on("channel.follow", () => {
-            notificationRecieved = true;
-        });
-        const payload = {
-            subscription: {
-                id: "f1c2a387-161a-49f9-a165-0f21d7a4e1c4",
-                type: "channel.follow",
-                version: "1",
-                condition: {
-                    broadcaster_user_id: "12826",
-                },
-                transport: {
-                    method: "webhook",
-                    callback: "https://example.com/webhooks/callback",
-                },
-                created_at: "2019-11-16T10:11:12.123Z",
-            },
-            event: {
-                user_id: "1337",
-                user_name: "awesome_user",
-                broadcaster_user_id: "12826",
-                broadcaster_user_name: "twitch",
-            },
-        };
-        const signature = crypto
-            .createHmac("sha256", whSecret)
-            .update("befa7b53-d79d-478f-86b9-120f112b044e" + timestamp + Buffer.from(JSON.stringify(payload), "utf-8"))
-            .digest("hex");
-        request(app)
-            .post("/teswh/event")
-            .set({
-                "Twitch-Eventsub-Message-Id": "befa7b53-d79d-478f-86b9-120f112b044e",
-                "Twitch-Eventsub-Message-Retry": 0,
-                "Twitch-Eventsub-Message-Type": "notification",
-                "Twitch-Eventsub-Message-Signature": `sha256=${signature}`,
-                "Twitch-Eventsub-Message-Timestamp": timestamp,
-                "Twitch-Eventsub-Subscription-Type": "channel.follow",
-                "Twitch-Eventsub-Subscription-Version": 1,
-            })
-            .send(payload)
-            .expect(200)
-            .end((err, res) => {
-                if (err) return done(err);
-                res.text.should.eq("OK");
-                notificationRecieved.should.eq(true);
-                done();
-            });
+    it("responds with 200 OK when receiving a notification and the event should be fired", async () => {
+        const cb = sinon.spy();
+        tes.on("channel.follow", cb);
+        await cmd(`twitch event trigger channel.follow -F ${REDIRECT_URL} -s ${whSecret}`);
+        sinon.assert.called(cb);
     });
 
+    // TODO: update when able to send a revocation notification in Twitch CLI
     it("responds with 200 OK when receiving a revocation and the revocation event should be fired", (done) => {
         let notificationRecieved = false;
         tes.on("revocation", () => {
@@ -179,65 +109,28 @@ describe("whserver", () => {
             })
             .send(payload)
             .expect(200)
-            .end((err, res) => {
+            .end((err, { text }) => {
                 if (err) return done(err);
-                res.text.should.eq("OK");
-                notificationRecieved.should.eq(true);
+                expect(text).to.eq("OK");
+                expect(notificationRecieved).to.eq(true);
                 done();
             });
     });
 
-    it("responds with 200 OK when receiving a duplicate notification and the event should not be fired", (done) => {
-        let notificationRecieved = false;
-        tes.on("channel.follow", () => {
-            notificationRecieved = true;
-        });
-        const payload = {
-            subscription: {
-                id: "f1c2a387-161a-49f9-a165-0f21d7a4e1c4",
-                type: "channel.follow",
-                version: "1",
-                condition: {
-                    broadcaster_user_id: "12826",
-                },
-                transport: {
-                    method: "webhook",
-                    callback: "https://example.com/webhooks/callback",
-                },
-                created_at: "2019-11-16T10:11:12.123Z",
-            },
-            event: {
-                user_id: "1337",
-                user_name: "awesome_user",
-                broadcaster_user_id: "12826",
-                broadcaster_user_name: "twitch",
-            },
+    it("responds with 200 OK when receiving a duplicate notification and the event should not be fired", async () => {
+        let resendID;
+        const cb = (_event, { id }) => {
+            resendID = id;
         };
-        const signature = crypto
-            .createHmac("sha256", whSecret)
-            .update("befa7b53-d79d-478f-86b9-120f112b044e" + timestamp + Buffer.from(JSON.stringify(payload), "utf-8"))
-            .digest("hex");
-        request(app)
-            .post("/teswh/event")
-            .set({
-                "Twitch-Eventsub-Message-Id": "befa7b53-d79d-478f-86b9-120f112b044e",
-                "Twitch-Eventsub-Message-Retry": 0,
-                "Twitch-Eventsub-Message-Type": "notification",
-                "Twitch-Eventsub-Message-Signature": `sha256=${signature}`,
-                "Twitch-Eventsub-Message-Timestamp": timestamp,
-                "Twitch-Eventsub-Subscription-Type": "channel.follow",
-                "Twitch-Eventsub-Subscription-Version": 1,
-            })
-            .send(payload)
-            .expect(200)
-            .end((err, res) => {
-                if (err) return done(err);
-                res.text.should.eq("OK");
-                notificationRecieved.should.eq(false);
-                done();
-            });
+        const spy = sinon.spy(cb);
+        tes.on("channel.follow", spy);
+
+        await cmd(`twitch event trigger channel.follow -F ${REDIRECT_URL} -s ${whSecret}`);
+        await cmd(`twitch event retrigger channel.follow -F ${REDIRECT_URL} -s ${whSecret} -i ${resendID}`);
+        sinon.assert.calledOnce(spy);
     });
 
+    // TODO: update when able to set timestamps in Twitch CLI
     it("responds with 200 OK when receiving an old notification and the event should not be fired", (done) => {
         let notificationRecieved = false;
         tes.on("channel.follow", () => {
@@ -282,10 +175,10 @@ describe("whserver", () => {
             })
             .send(payload)
             .expect(200)
-            .end((err, res) => {
+            .end((err, { text }) => {
                 if (err) return done(err);
-                res.text.should.eq("OK");
-                notificationRecieved.should.eq(false);
+                expect(text).to.eq("OK");
+                expect(notificationRecieved).to.eq(false);
                 done();
             });
     });
