6f88341f4ec402ef6349acdd7e1fd3193aa4cdf6
Mitchell Adair <mitchwadair@gmail.com>
2022-08-22 00:37:50 -0400

    improve events tests

--- test/events.js
+++ test/events.js
@@ -8,7 +8,11 @@ beforeEach(async () => {
 describe("EventManager", () => {
     it("should not add a listener which is passed a non-function handler", async () => {
         expect(EventManager._events["test"]).to.not.exist;
-        EventManager.addListener.bind("test", "not a function").should.throw(Error);
+        try {
+            EventManager.addListener("test", "not a function");
+        } catch (error) {
+            expect(error.message).to.eq("Event handler must be a function");
+        }
     });
 
     it("adds new event listeners", async () => {
@@ -16,7 +20,8 @@ describe("EventManager", () => {
         EventManager.addListener("test", () => {
             return;
         });
-        EventManager._events["test"].should.be.a("function");
+        expect(EventManager._events["test"]).to.exist;
+        expect(typeof EventManager._events["test"]).to.eq("function");
     });
 
     it("removes individual listeners correctly", async () => {
@@ -24,7 +29,8 @@ describe("EventManager", () => {
         EventManager.addListener("test", () => {
             return;
         });
-        EventManager._events["test"].should.be.a("function");
+        expect(EventManager._events["test"]).to.exist;
+        expect(typeof EventManager._events["test"]).to.eq("function");
         EventManager.removeListener("test");
         expect(EventManager._events["test"]).to.not.exist;
     });
@@ -43,10 +49,12 @@ describe("EventManager", () => {
         EventManager.addListener("test2", () => {
             return;
         });
-        EventManager._events["test"].should.be.a("function");
-        EventManager._events["test2"].should.be.a("function");
+        expect(EventManager._events["test"]).to.exist;
+        expect(typeof EventManager._events["test"]).to.eq("function");
+        expect(EventManager._events["test2"]).to.exist;
+        expect(typeof EventManager._events["test2"]).to.eq("function");
         EventManager.removeAllListeners();
-        EventManager._events.should.be.an("object").that.is.empty;
+        expect(EventManager._events).to.be.empty;
     });
 
     it("fires existing events correctly with correct arguments", async () => {
@@ -60,9 +68,9 @@ describe("EventManager", () => {
             arg1Actual = arg1;
             arg2Actual = arg2;
         });
-        EventManager.fire({ type: "test" }, argData).should.eq(true);
-        arg1Actual.should.eq("test arg1");
-        arg2Actual.should.eq("test arg2");
+        expect(EventManager.fire({ type: "test" }, argData)).to.eq(true);
+        expect(arg1Actual).to.eq("test arg1");
+        expect(arg2Actual).to.eq("test arg2");
     });
 
     it("does not fire unknown events", async () => {
@@ -71,6 +79,6 @@ describe("EventManager", () => {
             arg1: "test arg1",
             arg2: "test arg2",
         };
-        EventManager.fire({ type: "test" }, argData).should.eq(false);
+        expect(EventManager.fire({ type: "test" }, argData)).to.eq(false);
     });
 });
