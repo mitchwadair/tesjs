6c4a3c19b2302a3f8744e764735df9075dca47e3
Mitchell Adair <mitchwadair@gmail.com>
2022-10-31 21:35:36 -0400

    env test check cli version

--- test/test_environment.js
+++ test/test_environment.js
@@ -2,10 +2,10 @@ const { expect } = require("chai");
 const { cmd } = require("./testUtil");
 
 describe("Test Environment", () => {
-    it("has the twitch CLI installed", async () => {
+    it("has required twitch CLI installed", async () => {
         try {
             const out = await cmd("twitch version");
-            expect(out).to.not.be.undefined;
+            expect(out).to.contain("1.1.12");
         } catch (err) {
             throw new Error("Twitch CLI not installed");
         }
