ba1b9ae8940085d1f5900173ac1b35b2f7e1c1e9
Mitchell Adair <mitchwadair@gmail.com>
2022-11-11 00:44:51 -0500

    #33: add wsclient lifecycle test

--- test/tests.js
+++ test/tests.js
@@ -4,5 +4,6 @@ require("./logger");
 require("./request");
 require("./auth");
 require("./tes");
+require("./wsclient"); // this MUST be before whserver tests for some reason???
 require("./whserver");
 require("./events");
---
+++ test/wsclient.js
@@ -0,0 +1,44 @@
+const { spawn } = require("child_process");
+const sinon = require("sinon");
+const { expect } = require("chai");
+const WebSocketClient = require("../lib/wsclient");
+
+describe("wsclient", () => {
+    let server;
+    before((done) => {
+        server = spawn("twitch", ["event", "start-websocket-server", "--reconnect", "11"]);
+        setTimeout(done, 100);
+    });
+
+    after(() => {
+        server.kill("SIGINT");
+    });
+
+    it("correctly handles session lifecycle", (done) => {
+        const client = new WebSocketClient();
+        let firstSessionID;
+        const connection = client._addConnection((id) => (firstSessionID = id), "ws://localhost:8080/eventsub");
+        const messageSpy = sinon.spy(connection._events, "message");
+        const closeSpy = sinon.spy(connection._events, "close");
+        const addConnectionSpy = sinon.spy(client, "_addConnection");
+        setTimeout(() => {
+            const messageTypes = ["session_welcome", "session_keepalive", "session_reconnect"];
+            // `onmessage` should be called three times
+            expect(messageSpy.getCalls()).to.have.length(3);
+            // the `message_type` for each should come in the order of the array above
+            messageSpy.getCalls().forEach((call, index) => {
+                const messageType = JSON.parse(call.args[0].toString()).metadata?.message_type;
+                expect(messageType).to.eq(messageTypes[index]);
+            });
+            // `onclose` should be called on the original connection
+            sinon.assert.calledOnce(closeSpy);
+            // a new connection should be added on reconnect
+            sinon.assert.calledOnce(addConnectionSpy);
+            const sessionIDs = Object.keys(client._connections);
+            // when the new session is added and confirmed, the old one should be removed
+            expect(sessionIDs).to.have.length(1);
+            expect(firstSessionID).not.to.eq(sessionIDs[0]);
+            done();
+        }, 11500);
+    }).timeout(12000);
+});
