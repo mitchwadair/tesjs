1d6c0fe49e163f16c80fe0cd01c3a54e3a0549ff
Mitchell Adair <mitchwadair@gmail.com>
2022-11-11 23:45:04 -0500

    #33: add verify-checksum check, remove checsum to test

---
+++ scripts/verify-checksum.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+git config --local core.autocrlf true
+
+CHANGED=$(git diff --name-only | grep "checksum")
+
+if [[ -n "$CHANGED" ]]; then
+    echo "checksum changed from build, run 'npm run build' to commit new build"
+    exit 1
+else
+    exit 0
+fi
