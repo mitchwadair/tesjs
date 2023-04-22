1a1734edb9256bf492aa257d3f036e30afaac47b
Mitchell Adair <mitchwadair@gmail.com>
2023-04-21 23:30:54 -0400

    improve build check

---
+++ scripts/verify-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+git config --local core.autocrlf false
+git config --local core.safecrlf false
+
+CHANGED=$(git diff --name-only | grep -w "dist/tesjs.min.js")
+
+if [[ -n "$CHANGED" ]]; then
+    echo "build changed, run 'npm run build' to commit new build"
+    exit 1
+else
+    echo "builds match, build is up to date"
+    exit 0
+fi
