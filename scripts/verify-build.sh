712e68f1073327da7a7271e2a7393c29f0cc8c75
Mitchell Adair <mitchwadair@gmail.com>
2023-04-21 23:38:07 -0400

    fix verify script

--- scripts/verify-build.sh
+++ scripts/verify-build.sh
@@ -3,7 +3,7 @@
 git config --local core.autocrlf false
 git config --local core.safecrlf false
 
-CHANGED=$(git diff --name-only | grep -w "dist/tesjs.min.js")
+CHANGED=$(git diff --name-only | grep -w "dist/tes.min.js")
 
 if [[ -n "$CHANGED" ]]; then
     echo "build changed, run 'npm run build' to commit new build"
